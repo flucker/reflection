2988f1786fec0d975c17231b940394bf8fbd7814
Eugene Grigoryev <eugene1grigoryev@gmail.com>
2025-04-27 01:57:52 +0300

    hw

--- src/main/java/org/example/App2.java
+++ src/main/java/org/example/App2.java
@@ -1,39 +1,76 @@
 package org.example;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.util.Arrays;
 
+class Sample {
+    String sampleString = "xxx";
+}
 class Person {
-    int name = 111;
-    String surname = "Hello";
+    String name = "Eugene";
+    int age = 100;
+    int[][] arr = {{1, 2}, {3, 4}, {5, 6}};
+    Sample sample = new Sample();
 }
 
 public class App2 {
     public static void main(String[] args) throws IllegalAccessException {
         Person person = new Person();
 
-        Class<?> cls = Person.class;
-        StringBuilder sb = new StringBuilder();
+        System.out.println(objToJSON(person));
+    }
 
+    public static String objToJSON(Object obj) throws IllegalAccessException {
+        Class<?> cls = obj.getClass();
+        StringBuilder sb = new StringBuilder();
         Field[] fields = cls.getDeclaredFields();
 
         sb.append("{");
 
         for (Field field : fields) {
             field.setAccessible(true);
-            Object value = field.get(person);
+            Object value = field.get(obj);
+            if (field.getType().isArray()) {
+                value = objToJSONArray(value);
+            }  else if (value.getClass().getPackage().getName().equals(App2.class.getPackage().getName())) {
+                value = objToJSON(value);
+            } else {
+                value = "\"" + value + "\"";
+            }
 
             sb.append('"')
                     .append(field.getName())
                     .append('"')
                     .append(": ")
-                    .append('"')
                     .append(value)
-                    .append('"').append(',');
+                    .append(',');
 
         }
-
+        sb.deleteCharAt(sb.length() - 1);
         sb.append("}");
 
-        System.out.println(sb.toString());
+        return sb.toString();
+    }
+
+    public static String objToJSONArray(Object obj) throws IllegalAccessException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        for (int i = 0; i < Array.getLength(obj); i++) {
+            Object value = Array.get(obj, i);
+            if (value.getClass().isArray()) {
+                value = objToJSONArray(value);
+            } else if (value.getClass().getPackage().getName().equals(App2.class.getPackage().getName())) {
+                value = objToJSON(value);
+            } else {
+                value = "\"" + value + "\"";
+            }
+            sb.append(value).append(',');
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        sb.append("]");
+
+        return sb.toString();
     }
 }
